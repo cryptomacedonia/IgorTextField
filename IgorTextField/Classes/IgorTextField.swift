8f9c36605dcb4567ade2dd90dd8042a62253d57f
Igor Jovcevski <igor.jovcevski@sourcico.com>
2021-11-20 18:23:26 +0100

    pod create 1

---
+++ IgorTextField/Classes/IgorTextField.swift
@@ -0,0 +1,109 @@
+
+import RxCocoa
+
+
+extension  UITextField   {
+    enum ValidationRule {
+        case hasNumber(_ value:Bool)
+        case hasUpperCase(_ value:Bool)
+        case charactersCountMoreThan(_ numberOfCharacters:Int)
+        case hasSpecialCharacter(_ value:Bool)
+        case hasLowerCase(_ value:Bool)
+        case hasSpace(_ value:Bool)
+    }
+    func addValidation(validators:[ValidationRule], errorLabel:UILabel, generalErrorString:String? = nil) {
+       
+        self.rx.text.bind { text in
+            guard let text = text else {
+                return
+            }
+            var errorFound:Bool = false
+            validators.forEach {validator in
+                switch validator {
+                case .charactersCountMoreThan(let numberofcharacters):
+                    if text.count < numberofcharacters {
+                        errorFound = true
+                    }
+                    break;
+                case .hasNumber(let shouldHaveNumber):
+                    if hasNumbers(text) != shouldHaveNumber {
+                        errorFound = true
+                    }
+                    break;
+                case .hasUpperCase(let shouldHaveUpperCase):
+                    if hasUpperCase(text) != shouldHaveUpperCase {
+                        errorFound = true
+                    }
+                    break;
+                case .hasSpecialCharacter(let shouldHaveSpecialCharacter):
+                    if hasSpecialCharacter(text) != shouldHaveSpecialCharacter {
+                        errorFound = true
+                    }
+                    break;
+                case .hasLowerCase(let shouldHaveLowerCase):
+                    if hasLowerCase(text) != shouldHaveLowerCase {
+                        errorFound = true
+                    }
+                    break;
+                case .hasSpace(let shouldhaveSpace):
+                    if hasSpace(text) != shouldhaveSpace {
+                        errorFound = true
+                    }
+                    break;
+                }
+                if errorFound {
+                    errorLabel.isHidden = false
+                    errorLabel.text = generalErrorString
+                    errorLabel.superview!.superview!.layoutIfNeeded()
+                } else {
+                    errorLabel.isHidden = true
+                    errorLabel.text = ""
+                    errorLabel.superview!.superview!.layoutIfNeeded()
+                }
+            }
+        }
+        func hasNumbers(_ text:String) -> Bool {
+            for character in text{
+                if character.isNumber{
+                    return true
+                }
+            }
+            return false
+        }
+        func hasSpecialCharacter(_ text:String) -> Bool {
+            for character in text{
+                if character.isSymbol{
+                    return true
+                }
+            }
+            return false
+        }
+        func hasSpace(_ text:String) -> Bool {
+            for character in text{
+                if character.isWhitespace{
+                    return true
+                }
+            }
+            return false
+        }
+        func hasUpperCase(_ text:String)-> Bool {
+            
+            for character in text{
+                if character.isUppercase  {
+                    return true
+                }
+            }
+            return false
+        }
+        func hasLowerCase(_ text:String)-> Bool {
+            
+            for character in text{
+                if character.isLowercase  {
+                    return true
+                }
+            }
+            return false
+        }
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # IgorTextField
-Validation TextField
+
+[![CI Status](https://img.shields.io/travis/cryptomacedonia/IgorTextField.svg?style=flat)](https://travis-ci.org/cryptomacedonia/IgorTextField)
+[![Version](https://img.shields.io/cocoapods/v/IgorTextField.svg?style=flat)](https://cocoapods.org/pods/IgorTextField)
+[![License](https://img.shields.io/cocoapods/l/IgorTextField.svg?style=flat)](https://cocoapods.org/pods/IgorTextField)
+[![Platform](https://img.shields.io/cocoapods/p/IgorTextField.svg?style=flat)](https://cocoapods.org/pods/IgorTextField)
+
+## Example
+
+To run the example project, clone the repo, and run `pod install` from the Example directory first.
+
+## Requirements
+
+## Installation
+
+IgorTextField is available through [CocoaPods](https://cocoapods.org). To install
+it, simply add the following line to your Podfile:
+
+```ruby
+pod 'IgorTextField'
+```
+
+## Author
+
+cryptomacedonia, igor.jovcevski@sourcico.com
+
+## License
+
+IgorTextField is available under the MIT license. See the LICENSE file for more info.
